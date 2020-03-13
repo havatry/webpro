97821c78615c75211604bee51518880aac4f508d
havatry <zylttr@163.com>
2020-03-14 03:07:04 +0800

    添加等待请求结果时ajax显示信息

--- src/resources/static/js/commicate.js
+++ src/resources/static/js/commicate.js
@@ -42,7 +42,7 @@ function RequestPostX(url, data, func) {
     });
 }
 
-function RequestGet(url, func_succ) {
+function RequestGetPage(dom, url, func_succ) {
     $.ajax({
         type: "GET",
         url: url,
@@ -50,9 +50,15 @@ function RequestGet(url, func_succ) {
         contentType: false,
         processData: false,
         xhrFields: {withCredentials: true},
+        beforeSend: function () {
+            dom.innerHTML = '<h3 style="text-align:center; color:rgb(64, 224, 208); margin-top: 100px; margin-bottom: 100px">请求数据中...</h3>';
+        },
         success: func_succ,
         error: function(e){
-            alert(JSON.stringify(e));
+            // alert(JSON.stringify(e));
+            dom.innerHTML = '';
+            alert('无法获取到请求数据，请检查网络连接或者联系管理员');
+            // dom.innerHTML = '<h3 style="text-align:center; color:rgb(186, 85, 211); margin-top: 100px; margin-bottom: 100px">未能获取到请求数据</h3>'
         }
     });
 }
