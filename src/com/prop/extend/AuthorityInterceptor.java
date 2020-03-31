654b225ee290d34f81aea9496ec6cde6ad96cf5d
hudedong <hudedong@corp.netease.com>
2020-03-31 20:39:49 +0800

    1. 增加登陆才能操作查询历史请求的功能 使用WebFilter注解

---
+++ src/com/prop/extend/AuthorityInterceptor.java
@@ -0,0 +1,38 @@
+package com.prop.extend;
+
+import com.alibaba.fastjson.JSON;
+import com.prop.util.Process;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+@WebFilter(value = {"/search/*"}, filterName = "restrictExperiment")
+public class AuthorityInterceptor implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        // 判断用户是否登陆
+        String uid = Process.parseUid((HttpServletRequest)servletRequest, (HttpServletResponse)servletResponse);
+        if (uid.indexOf("session_") >= 0) {
+            // 登陆状态
+            filterChain.doFilter(servletRequest, servletResponse); // 放行
+        } else {
+            // 非登陆
+            Process.setResp((HttpServletResponse)servletResponse, ((HttpServletRequest)servletRequest).getHeader("Origin"));
+            PrintWriter out = servletResponse.getWriter();
+            out.println(JSON.toJSONString(JSON.parseObject("{msg:'若要执行查询操作, 请先登陆'}")));
+            out.close();
+        }
+    }
+
+    @Override
+    public void destroy() {}
+}
