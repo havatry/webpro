2821fd6d606725be1e34defe668225076ba1d007
hudedong <hudedong@corp.netease.com>
2020-04-05 19:19:57 +0800

    在config/setting.js中配置是否开启登陆功能 代替ip的判断，因为localhost在docker之间通信将不适用

--- resources/static/config/setting.js
+++ resources/static/config/setting.js
@@ -2,3 +2,4 @@ var server_host = '175.24.101.52'; // 部署服务器的主机名或者ip
 var server_port = 8080; // 部署服务器的端口
 var uri_prefix = 'webpro'; // 部署项目的名称
 var url_scheme = 'http'; // 部署服务器使用的协议
+var login_on = true; // 是否开启登陆功能，由于浏览器不能存储跨站cooike，当server_host的ip和本机ip不一致时候，请将该变量设置为false
